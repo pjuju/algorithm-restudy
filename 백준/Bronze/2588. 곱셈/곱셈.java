570df6b3f4a1f4e9032c04fd64515c1378a0d499
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2022-12-14 01:05:17 +0900

    [Bronze III] Title: 곱셈, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        sc.close();
+
+        int C = A * (B % 10);
+        int D = A * (B % 100 / 10);
+        int E = A * (B / 100);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+        System.out.println(A*B);
+
+    }
+}
