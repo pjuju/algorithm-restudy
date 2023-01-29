6763d9fee55f4f34da2ca59054607f4f0347dc4e
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-01-29 18:58:40 +0900

    [Gold IV] Title: 오큰수, Time: 1112 ms, Memory: 149900 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,15 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 nums = list(map(int,input().split()))
+result = [-1] * N
 
-ans = [-1] * N
 stack = []
 
 for i in range(N):
     while stack:
         if nums[stack[-1]] < nums[i]:
-            ans[stack.pop()] = nums[i]
+            result[stack.pop()] = nums[i]
         else:
             break
     stack.append(i)
-
-print(*ans)
+    
+print(*result)
+    
