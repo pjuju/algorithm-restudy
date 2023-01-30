fa0d51312328f4886abbb2dbaf6e331690ea2a39
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-01-30 16:29:37 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 126408 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,8 +1,12 @@
+import sys
 from collections import deque
-d = deque()
-for i in range(int(input())):
-    d.append(i+1)
-while len(d) > 1:
-    d.popleft()
-    d.append(d.popleft())
-print(*d)
+input = sys.stdin.readline
+
+N = int(input())
+queue = deque([i for i in range(1, N+1)])
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+
+print(queue[0])
