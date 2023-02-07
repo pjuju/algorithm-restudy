b46940a7448ceebeef560447fc9740cca7ee730b
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-02-07 16:55:35 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 608 ms, Memory: 180472 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,23 +1,24 @@
 N, M = map(int, input().split())
+
 graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+result = 0
+
 for _ in range(M):
-    u, v = map(int, input().split())
+    u,v = map(int, input().split())
     graph[u].append(v)
     graph[v].append(u)
     
-visited = [False] * (N+1)
-count = 0
-
-def dfs(x):
-    visited[x] = True
-    for y in graph[x]:
-        if not visited[y]:
-            dfs(y)
-    
+def dfs(node):
+    for next in graph[node]:
+        if not visited[next]:
+            visited[next] = 1
+            dfs(next)
+            
 for i in range(1, N+1):
     if not visited[i]:
-        count += 1
+        visited[i] = 1
+        result += 1
         dfs(i)
         
-print(count)
-        
+print(result)
