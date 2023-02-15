14083ee3d0e61e48078ae62b970a9c7cdf03a35d
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-02-15 14:29:44 +0900

    [Silver I] Title: 회의실 배정, Time: 324 ms, Memory: 122480 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,22 +1,20 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
 N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-arr.sort(key=lambda x:(x[1],x[0]), reverse=True)
+arr = []
 
-S,E = arr[0]
-cnt = 1
-for i in range(1,N):
-    ss, ee = arr[i]
+for _ in range(N):
+    start, end = map(int, input().split())
+    heapq.heappush(arr, (end, start))
 
-    if S >= ee:
-        cnt += 1
+now = 0
+cnt = 0
+while arr:
+    end, start = heapq.heappop(arr)
+    if start >= now:
+       cnt += 1
+       now = end
 
-        S, E = ss, ee
-
-
-    elif S <= ss:
-        S, E = ss, ee
-
-
-print(cnt)
-
-
+print(cnt)  
