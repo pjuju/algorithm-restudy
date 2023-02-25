d77e4fd519660a8ebe2dca182bde512159a08483
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-02-25 23:08:08 +0900

    [Gold IV] Title: 이분 그래프, Time: 1428 ms, Memory: 51684 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,28 +1,42 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+# 그래프의 정점을 둘로 분할, 각 집합에 속한 정점끼리는 서로 인접하지 않는다 = 이분 그래프
+# def bfs(i):
+
+
 T = int(input())
-for tc in range(1, T+1):
-    V, E = map(int, input().split())
+for _ in range(T):
+    V, E = map(int ,input().split())
+    
     graph = [[] for _ in range(V+1)]
-    color = [0] * (V+1)
     
     for _ in range(E):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a)
+        u,v = map(int,input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    graph_sep = [0] * (V+1)
 
     result = 'YES'
     for i in range(1, V+1):
-        if not color[i] and result == 'YES':
-            color[i] = 1
-            queue = [i]
-            while queue and result == 'YES':
-                ni = queue.pop(0)
-                for l in graph[ni]:
-                    if not color[l]:
-                        color[l] = -color[ni]
-                        queue.append(l)
-                    elif color[l] == color[ni]:
-                        result = 'NO'
+        if (result == 'YES') and (not graph_sep[i]):
+            
+            queue = deque()
+            graph_sep[i] = 1
+            queue.append(i)
+
+            while queue:
+                now = queue.popleft()
+                color = graph_sep[now]
+                for next in graph[now]:                    
+                    if not graph_sep[next]:
+                        graph_sep[next] = -color
+                        queue.append(next)
+                    
+                    elif graph_sep[next] == color:
+                        result = "NO"
                         break
-
+                        
     print(result)
-
