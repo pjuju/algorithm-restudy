8895140e2e17b81d95fd226dcd33e738bb4e3c9e
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-03-03 00:05:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 324 ms, Memory: 56484 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,31 +1,31 @@
+import sys
+input = sys.stdin.readline
 import heapq
 
 N = int(input())
 M = int(input())
+INF = 0xfffffff
 
 graph = [[] for _ in range(N+1)]
-
 for _ in range(M):
-    a,b,c = map(int, input().split())
-    graph[a].append((c,b))
+    a, b, w = map(int, input().split()) 
+    graph[a].append((b,w))
 
 start, end = map(int, input().split())
+costs = [INF] * (N+1)
+costs[start] = 0
+hq = []
+heapq.heappush(hq,(0, start))
 
-INF = 0xfffffff
-distance = [INF] * (N+1)
-distance[start] = 0
-heap = []
-heapq.heappush(heap, [0, start])
-
-while heap:
-    cost, to = heapq.heappop(heap)
-    if distance[to] < cost:
+while hq:
+    cost, now = heapq.heappop(hq)
+    if costs[now] < cost:
         continue
-    for new_cost, new_to in graph[to]:
-        if distance[new_to] > cost+new_cost:
-            distance[new_to] = cost+new_cost
-            heapq.heappush(heap, [cost+new_cost, new_to])
 
-print(distance[end])
+    for next, w in graph[now]:
+        if costs[next] > cost+w:
+            costs[next] = cost+w
+            heapq.heappush(hq, (cost+w, next))
 
+print(costs[end])
 
