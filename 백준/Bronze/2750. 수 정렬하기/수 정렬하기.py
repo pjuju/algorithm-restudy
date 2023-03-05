6f6a2d68601618c6efc089c9b0d1c524141c26e5
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-03-05 20:51:06 +0900

    [Bronze II] Title: 수 정렬하기, Time: 148 ms, Memory: 115156 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,12 +1,12 @@
 N = int(input())
-nums = [int(input()) for _ in range(N)]
+arr = [int(input()) for _ in range(N)]
 
-for i in range(N):
+for i in range(N-1):
     for j in range(N-1-i):
-        if nums[j] > nums[j+1]:
-            temp = nums[j]
-            nums[j] = nums[j+1]
-            nums[j+1] = temp
-
-for num in nums:
+        if arr[j] > arr[j+1]:
+            temp = arr[j+1]
+            arr[j+1] = arr[j]
+            arr[j] = temp
+            
+for num in arr:
     print(num)
