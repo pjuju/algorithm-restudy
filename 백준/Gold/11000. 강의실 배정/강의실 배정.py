bed1a48ce0643237170b91caaca45d5fb8728e8e
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-03-22 00:55:09 +0900

    [Gold V] Title: 강의실 배정, Time: 576 ms, Memory: 61296 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+import heapq
+
+N = int(input())
+hq = []
+for _ in range(N):
+    s,e = map(int, input().split())
+    heapq.heappush(hq, (s,e))
+
+room = [0]
+while hq:
+    s,e = heapq.heappop(hq)
+    
+    min_time = heapq.heappop(room)
+    heapq.heappush(room,e)
+
+    if s < min_time:
+        heapq.heappush(room,min_time)
+    
+print(len(room))
