90d4e3adbc774fe73a4f5bbfe0b385e8507a5d35
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-03-21 21:36:09 +0900

    [level 3] Title: 양과 늑대, Time: 0.35 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/lv3/92343. 양과 늑대/양과 늑대.py
@@ -1,23 +1,32 @@
+answer = 0
+
+
+    
 
 
 def solution(info, edges):
+    graph = [[] for _ in range(len(info))]
+    for s,e in edges:
+        graph[s].append(e)
+    
     visited = [0] * len(info)
-    answer = []
-    def dfs(sheep, wolf):
-        if sheep > wolf:
-            answer.append(sheep)
-        else:
-            return 
-        
-        for p, c in edges:
-            if visited[p] and not visited[c]:
-                visited[c] = 1
-                if info[c] == 0:
-                    dfs(sheep+1, wolf)
-                else:
-                    dfs(sheep, wolf+1)
-                visited[c] = 0
     visited[0] = 1
-    dfs(1, 0)
-
-    return max(answer)
+    
+    result = []
+    
+    def dfs(sheep, wolf):
+        if wolf >= sheep:
+            return
+        result.append(sheep)
+        
+        for s,e in edges:
+            if visited[s] and not visited[e]:
+                visited[e] = 1
+                if info[e]:
+                    dfs(sheep, wolf+1)
+                else:
+                    dfs(sheep+1, wolf)
+                visited[e] = 0
+    dfs(1,0)
+    
+    return max(result)
