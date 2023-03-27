e3e1931ef75d924eddce5c60f99409aceb967b6d
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-03-27 12:05:46 +0900

    [unrated] Title: 미로 탈출 명령어, Time: 0.77 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/unrated/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,60 +1,20 @@
 from collections import deque
-
-
-
 def solution(n, m, x, y, r, c, k):
-    have_to_move = [0] * 4 # 하, 좌, 우 상 
-    answer = ''
-    t = ['d','l','r','u']
-    td = r-x
-    lr = c-y
+    answer = 'z'*2500
+    dq = deque([(x, y, '', 0)])
+    dir2char = {(1, 0): 'd', (0, -1): 'l', (0, 1): 'r', (-1, 0):'u'}
     
-    if td > 0:
-        have_to_move[0] += td
-    else:
-        have_to_move[3] -= td
-    if lr > 0:
-        have_to_move[2] += lr
-    else:
-        have_to_move[1] -= lr
-    
-    while sum(have_to_move) < k:
-        # print(answer,k,have_to_move,x,y)
-        
-        # 아래로 갈 수 있다면
-        k -= 1
-        if x < n:
-            x += 1            
-            answer += t[0]
-            if have_to_move[0]:
-                have_to_move[0] -= 1
-            else:
-                have_to_move[3] += 1
-        elif y > 1:
-            y -= 1
-            answer += t[1]
-            if have_to_move[1]:
-                have_to_move[1] -= 1
-            else:
-                have_to_move[2] += 1
-                
-        elif y < m:
-            y += 1
-            answer += t[2]
-            if have_to_move[2]:
-                have_to_move[2] -= 1
-            else:
-                have_to_move[1] += 1
-        
-    print(answer, k, have_to_move)
-    
-    if sum(have_to_move) != k:
-        return 'impossible'
-
-        
-    for x in range(4):
-        answer += (t[x] * have_to_move[x])
-    return answer
-
-
-
+    while dq:
+        x, y, direction, cnt = dq.popleft()
+        if direction > answer: continue
+        if cnt>= k:
+            if x == r and y == c:
+                answer = min(answer, direction)
+            continue
+        for dx, dy in [(1, 0), (0, -1), (0, 1), (-1, 0)]:
+            nx, ny = x+dx, y+dy
+            if 0<nx<=n and 0<ny<=m:
+                tmp_dir = direction+dir2char[(dx, dy)]
+                dq.append((nx, ny, tmp_dir, cnt+1))
+                break
+    return answer if answer != 'z'*2500 else 'impossible'
