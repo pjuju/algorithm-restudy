fe67dfe39aff26a6425e9e15026873437946a10e
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-03-30 12:37:39 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 120 ms, Memory: 43820 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -1,32 +1,17 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-arr = list(map(int, input().split()))
-result = 0
-visited = [0] * (100001)
-def func(n):
-    return (1+n)*(n/2)
-
-lst = deque()
-for i in range(N):
-    if not visited[arr[i]]:
-        lst.append(arr[i])
-        visited[arr[i]] = 1
-    else:
-        x = len(lst)
-        result += func(x)
-        cnt = 0
-        while True:
-            cnt += 1
-            a = lst.popleft()
-            if a == arr[i]:
-                break            
-            visited[a] = 0
-        result -= func(x-cnt)
-        lst.append(arr[i])
-        
-
-result += func(len(lst))
-print(int(result))
+n = int(input())
+nums = list(map(int, input().split()))
+cnt, start, end = 0, 0, 0
+seq = set()
+while end <= n-1:
+    if nums[end] not in seq:
+        seq.add(nums[end])
+        end += 1 
+        continue
+    cnt += len(seq)
+    seq.remove(nums[start])
+    start += 1
+cnt += len(seq) * (len(seq)+ 1) // 2
+print(cnt)
