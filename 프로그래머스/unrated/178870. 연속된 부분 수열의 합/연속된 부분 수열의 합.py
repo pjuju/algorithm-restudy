0ce45aa3bdf5b9584a6301f11e41e3f4c576b1e5
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-04-17 16:29:14 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -1,22 +1,15 @@
 def solution(sequence, k):
-    
-    result_left, result_right = 0,len(sequence)    
+    result_left = 0
+    result_right = len(sequence)
     left = 0
-    
     val = 0
-    
     for right in range(len(sequence)):
         val += sequence[right]
-        while val > k and left < right:
+        while val > k:
             val -= sequence[left]
             left += 1
         if val == k:
             if result_right - result_left > right - left:
                 result_left, result_right = left, right
-    
-    
-    
-    
-    
     
     return [result_left, result_right]
