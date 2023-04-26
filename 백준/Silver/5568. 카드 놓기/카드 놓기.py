da5b9c2f03b4f757543a4fab030e2441a965845c
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-04-26 10:50:54 +0900

    [Silver IV] Title: 카드 놓기, Time: 44 ms, Memory: 31768 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.py
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.py
@@ -3,14 +3,11 @@ k = int(input())
 cards = [input() for _ in range(n)]
 
 visited = [0] * n
-check = []
-result = 0
+result = set()
 def perm(x,word):
-    global result
+    
     if x == k:
-        if word not in check:
-            result += 1
-            check.append(word) 
+        result.add(word)
         return
     
     for i in range(n):
@@ -20,4 +17,4 @@ def perm(x,word):
             visited[i] = 0
               
 perm(0,'')
-print(result)
+print(len(result))
