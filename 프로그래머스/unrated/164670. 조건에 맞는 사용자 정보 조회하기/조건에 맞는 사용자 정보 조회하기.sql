66b18c339682e5f319793f41e4b0d5037968b365
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-04-26 20:17:53 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT user_id, nickname, CONCAT(CITY,' ',STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소', 
+CONCAT(SUBSTRING(TLNO,1,3),'-',SUBSTRING(TLNO,4,4) ,'-',SUBSTRING(TLNO,8,4))AS '전화번호'
+FROM USED_GOODS_USER AS user
+LEFT JOIN USED_GOODS_BOARD AS board
+ON user.user_id = board.writer_id
+GROUP BY user_id HAVING count(BOARD_ID) >= 3
+ORDER BY USER_ID DESC;
