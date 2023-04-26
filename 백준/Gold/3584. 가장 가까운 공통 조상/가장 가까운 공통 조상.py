3a012eaaa1f5219cb7b2f0400bdf2673a48a6544
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-04-26 10:15:27 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 1536 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -2,23 +2,28 @@ T = int(input())
 
 for tc in range(T):    
     N = int(input())
-    parents = [i for i in range(N+1)]
+    parents= [0] * (N + 1)
     for _ in range(N-1):
         a,b = map(int, input().split())
         parents[b] = a
     
     x,y = map(int, input().split())
-    x_lst = [x]    
-    while True:        
-        if x == parents[x]:
-            break
-        
+    x_lst = [x]   
+    y_lst = [y]
+     
+    while parents[x]:                
         x_lst.append(parents[x])
         x = parents[x]
     
-    while True:       
-        if y in x_lst:
-            print(y)
-            break
-        
+    while parents[y]:                
+        y_lst.append(parents[y])
         y = parents[y]
+    
+    x_level = len(x_lst) - 1
+    y_level = len(y_lst) - 1
+
+    while x_lst[x_level] ==  y_lst[y_level]:
+        x_level -= 1
+        y_level -= 1
+    
+    print(x_lst[x_level+1])
