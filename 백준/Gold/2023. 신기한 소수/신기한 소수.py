0e0aed2d4d7899da418e774f675bbecf6885be07
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-04-27 12:14:07 +0900

    [Gold V] Title: 신기한 소수, Time: 6996 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,22 +1,21 @@
 N = int(input())
 
-def is_prime(x):
-    for i in range(2, int(x/2)+1):
-        if x % i == 0:
+def dfs(num):
+    if len(str(num)) == N:
+        print(num)
+        return
+    
+    for j in range(1,10,2):
+        next = int(str(num) + str(j))
+        if isPrime(next):
+            dfs(next)
+
+def isPrime(x):
+    for i in range(2,x//2+1):
+        if x%i == 0:
             return False
     return True
 
-def dfs(n):
-    if len(str(n)) == N:
-        print(n)
-        return
-    
-    for i in range(0, 5):
-        i = i*2 + 1
-        next = int(str(n) + str(i))
-        if is_prime(next):
-            dfs(next)
-
-sosus = [2,3,5,7]      
+sosus = [2,3,5,7]
 for sosu in sosus:
     dfs(sosu)
