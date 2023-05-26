d3449dd5d43c2c22924db66942ffca6348bfb4d9
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-05-26 09:17:31 +0900

    [Gold V] Title: 동전 2, Time: 388 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,15 +1,15 @@
+import sys
+input = sys.stdin.readline
 n, k = map(int, input().split())
-dp = [0xfffffff] * (k+1)
-dp[0] = 0
 
 coins = [int(input()) for _ in range(n)]
 
-for num in coins:
-    for i in range(num, k+1):
-        if dp[i-num] != 0xfffffff:
-            dp[i] = min(dp[i], dp[i-num]+1)
-            
-if dp[-1] == 0xfffffff:
-    print(-1)
-else:
-    print(dp[-1])
+dp = [k+1] * (k+1)
+dp[0] = 0
+for coin in coins:
+    for x in range(coin, k+1):
+        dp[x] = min(dp[x], dp[x-coin]+1)
+
+
+print(-1 if dp[-1] == k+1 else dp[-1])
+
