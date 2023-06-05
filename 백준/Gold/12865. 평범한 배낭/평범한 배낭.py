a983760fcd291e484c9d513a1ae3acaae4f391e1
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-06-05 13:27:00 +0900

    [Gold V] Title: 평범한 배낭, Time: 2036 ms, Memory: 35108 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,38 +1,14 @@
-# N, K = map(int, input().split())
-# values = [0]*N
-# weights = [0]*N
-# result = 0
-#
-# for i in range(N):
-#     W, V = map(int, input().split())
-#     weights[i] = W
-#     values[i] = V
-#
-# def func(idx, val, wei):
-#     global result
-#
-#     if idx == N:
-#         if val > result :
-#             result = val
-#         return
-#     if wei + weights[idx] <= K:
-#         func(idx+1, val+values[idx], wei+weights[idx])
-#     func(idx+1, val, wei)
-#
-# func(0,0,0)
-# print(result)
+import sys
+input = sys.stdin.readline
 
-
-
-N,K = map(int,input().split())
-items = []
-for _ in range(N):
-    w,v = map(int,input().split())
-    items.append((w,v))
+N, K = map(int, input().split())
 dp = [0] * (K+1)
-# 무게별 최대 가치
-for item in items:
-    w,v = item
-    for i in range(K,w-1,-1):
-        dp[i] = max(dp[i],dp[i-w]+v)
+
+for _ in range(N):
+    w,v = map(int, input().split())
+
+    for x in range(K, w-1, -1):
+        if dp[x-w] + v > dp[x]:
+            dp[x] = dp[x-w] + v 
+            
 print(dp[-1])
