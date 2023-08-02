c128c06c5ea7b45e42c960d54c04b0227adf564e
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-08-02 14:12:50 +0900

    [Silver II] Title: 소-난다!, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/19699. 소－난다！/소－난다！.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+lst = []
+def func(i,val,cnt):
+    if cnt == M:
+        lst.append(val)
+    
+    for j in range(i+1, N):
+        func(j, val+nums[j], cnt+1)
+
+func(-1,0,0)
+lst = sorted(list(set(lst)))
+
+prime_list = [0] * (max(lst)+1)
+for i in range(2, max(lst)//2+1):
+    for j in range(2, max(lst)//i+1):
+        prime_list[i*j] = 1
+
+answer = []
+for x in lst:
+    if not prime_list[x]:
+        answer.append(x)
+
+if answer:
+    print(*answer)
+else:
+    print(-1)
+
