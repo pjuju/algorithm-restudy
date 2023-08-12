e2f68da7330444f2e79f874cf8a042e2c936ac7d
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-08-12 11:47:47 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 388 ms, Memory: 49400 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,18 +1,24 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
 N = int(input())
 graph = [[] for _ in range(N+1)]
+parents = [0] * (N+1)
+parents[1] = 1
 for _ in range(N-1):
-    a, b = map(int, input().split())
+    a,b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-parents = [0] * (N+1)
-queue = [1]
-while queue:
-    a = queue.pop(0)
-    for i in graph[a]:
-        if i in graph[a] and not parents[i]:
-            queue.append(i)
-            parents[i] = a
+dq = deque()
+dq.append(1)
+while dq:
+    now = dq.popleft()
+    for next in graph[now]:
+        if not parents[next]:
+            parents[next] = now
+            dq.append(next)
 
-for i in range(2,len(parents)):
+for i in range(2, N+1):
     print(parents[i])
