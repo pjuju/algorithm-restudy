ff9c7c5603ec73bc0e68fc030178d9a55fae9349
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-09-11 17:11:08 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1008 ms, Memory: 105376 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,19 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+arr = [[0]* (N+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
+sub_sum = [[0] * (N+1) for _ in range(N+1)]
 
-arr = [[0]*(N+1)]
-D = [[0]*(N+1) for _ in range(N+1)] 
-
-for i in range(N):
-    arr_row = [0] + [int(x) for x in input().split()]
-    arr.append(arr_row)
-    
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        D[i][j] = arr[i][j] + D[i-1][j] + D[i][j-1] - D[i-1][j-1]
+for r in range(1, N+1):
+    for c in range(1, N+1):
+        sub_sum[r][c] = sub_sum[r-1][c] + sub_sum[r][c-1] - sub_sum[r-1][c-1] + arr[r][c]
 
 for _ in range(M):
-    x1,y1,x2,y2 = map(int, input().split())
-    result = D[x2][y2] - D[x2][y1-1] - D[x1-1][y2] + D[x1-1][y1-1]
+    r1, c1, r2, c2 = map(int, input().split())
+    result = sub_sum[r2][c2] - sub_sum[r1-1][c2] - sub_sum[r2][c1-1] + sub_sum[r1-1][c1-1]
     print(result)
+
