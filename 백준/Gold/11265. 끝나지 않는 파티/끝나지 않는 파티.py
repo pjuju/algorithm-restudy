3bd2851ebadf8f59511e483854004ce0dfa94e9a
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-09-14 18:12:58 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 5752 ms, Memory: 34324 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            # i에서 j
+            if arr[i][j] > arr[i][k] + arr[k][j]:
+                arr[i][j] = arr[i][k] + arr[k][j]
+
+for _ in range(M):
+    A,B,C = map(int, input().split())
+    if arr[A-1][B-1] <= C:
+        print('Enjoy other party')
+    else:
+        print('Stay here')
