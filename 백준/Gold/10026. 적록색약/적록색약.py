4a2e1570667932a9d4f04b71ed14df25c8f3f752
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-09-22 13:42:24 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,50 +1,38 @@
 import sys
-sys.setrecursionlimit(10**5)
-
-dr, dc = [1,-1,0,0], [0,0,1,-1]
-
-def rgb(i,j):
-    rgb_visited[i][j] = 1
-    for x in range(4):
-        ni,nj = i+dr[x], j+dc[x]
-        if 0<=ni<N and 0<=nj<N and not rgb_visited[ni][nj]:
-            if arr[ni][nj] == arr[i][j]:
-                rgb(ni,nj)
-
-
-def jrsy(i,j):
-    jrsy_visited[i][j] = 1
-    for x in range(4):
-        ni, nj = i + dr[x], j + dc[x]
-        if 0 <= ni < N and 0 <= nj < N and not jrsy_visited[ni][nj]:
-            if not (arr[ni][nj]+arr[i][j])%2:
-                jrsy(ni, nj)
+sys.setrecursionlimit(10**9)
+input = sys.stdin.readline
+from collections import deque
 
 N = int(input())
 arr = [list(input()) for _ in range(N)]
-rgb_visited = [[0]*N for _ in range(N)]
-jrsy_visited = [[0]*N for _ in range(N)]
-rgb_cnt = 0
-jrsy_cnt = 0
+new_arr = [[0] * N for _ in range(N)]
+result = [0,0]
 
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 'R':
-            arr[i][j] = 0
-        elif arr[i][j] == 'G':
-            arr[i][j] = 2
-        elif arr[i][j] == 'B':
-            arr[i][j] = 1
-
-
-for i in range(N):
-    for j in range(N):
-        if not rgb_visited[i][j]:
-            rgb_cnt += 1
-            rgb(i,j)
-        if not jrsy_visited[i][j]:
-            jrsy_cnt += 1
-            jrsy(i,j)
+dr, dc = [0,0,-1,1], [-1,1,0,0]
+def func(r,c,array):
+    visited[r][c] = 1
+    for x in range(4):
+        nr, nc = r+dr[x], c+dc[x]
+        if 0<=nr<N and 0<=nc<N:
+            if array[nr][nc] == array[r][c]:
+                if not visited[nr][nc]:
+                    func(nr,nc,array)
 
 
-print(f'{rgb_cnt} {jrsy_cnt}')
+visited = [[0] * N for _ in range(N)]
+for r in range(N):
+    for c in range(N):
+        if arr[r][c] == 'B':
+            new_arr[r][c] = 1
+        if not visited[r][c]:
+            result[0] += 1
+            func(r,c,arr)
+
+visited = [[0] * N for _ in range(N)]
+for r in range(N):
+    for c in range(N):
+        if not visited[r][c]:
+            result[1] += 1
+            func(r,c,new_arr)
+
+print(*result)
