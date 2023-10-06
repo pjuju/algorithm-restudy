b22f4e1848bec29ccddd64f3f0d1b28dd481d110
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-10-06 18:29:47 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,39 +1,17 @@
--- 코드를 입력하세요
-
-SELECT h.history_id, TRUNCATE((DATEDIFF(h.end_date,h.start_date)+1) * c.daily_fee,0) as FEE
+SELECT h.history_id, TRUNCATE(c.daily_fee * (DATEDIFF(h.end_date, h.start_date)+1) *(100-IFNULL(p.discount_rate, 0)) / 100, 0) as FEE
+                                                        
 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-LEFT JOIN CAR_RENTAL_COMPANY_CAR as c
-ON h.car_id = c.car_id
-WHERE DATEDIFF(h.end_date,h.start_date)+1 < 7 and c.car_type = '트럭'
-UNION ALL
-
-SELECT h.history_id, TRUNCATE(((DATEDIFF(h.end_date,h.start_date)+1) * c.daily_fee * (100-p.discount_rate) / 100),0) as FEE
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-LEFT JOIN CAR_RENTAL_COMPANY_CAR as c
-ON h.car_id = c.car_id
+JOIN CAR_RENTAL_COMPANY_CAR as c
+ON c.car_id = h.car_id
 LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-ON p.car_type = c.car_type and p.duration_type ='7일 이상'
-WHERE 7 <= DATEDIFF(h.end_date,h.start_date)+1 and DATEDIFF(h.end_date,h.start_date)+1 < 30  and c.car_type = '트럭'
-
-UNION ALL
-
-SELECT h.history_id, TRUNCATE(((DATEDIFF(h.end_date,h.start_date)+1) * c.daily_fee * (100-p.discount_rate) / 100),0) as FEE
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-LEFT JOIN CAR_RENTAL_COMPANY_CAR as c
-ON h.car_id = c.car_id
-LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-ON p.car_type = c.car_type and p.duration_type ='30일 이상'
-WHERE 30 <= DATEDIFF(h.end_date,h.start_date)+1 and DATEDIFF(h.end_date,h.start_date)+1 < 90 and c.car_type = '트럭'
-
-UNION ALL
-
-SELECT h.history_id, TRUNCATE(((DATEDIFF(h.end_date,h.start_date)+1) * c.daily_fee * (100-p.discount_rate) / 100),0) as FEE
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY as h
-LEFT JOIN CAR_RENTAL_COMPANY_CAR as c
-ON h.car_id = c.car_id
-LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN as p
-ON p.car_type = c.car_type and p.duration_type ='90일 이상'
-WHERE 90 <= DATEDIFF(h.end_date,h.start_date)+1 and c.car_type = '트럭'
-
-
-ORDER BY FEE DESC, history_id DESC
+ON p.car_type = c.car_type and (CASE 
+                                WHEN DATEDIFF(h.end_date, h.start_date)+1 >= 90
+                                THEN '90일 이상'
+                                WHEN DATEDIFF(h.end_date, h.start_date)+1 >= 30
+                                THEN '30일 이상'
+                                WHEN DATEDIFF(h.end_date, h.start_date)+1 >= 7
+                                THEN '7일 이상'
+                                END)
+                                = p.duration_type
+WHERE c.car_type = '트럭'                           
+ORDER BY FEE desc, h.history_id desc
