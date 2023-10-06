d5d53b792bb9c73a2f2901b007ed10d978ed3f19
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-10-06 20:56:24 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,6 @@
--- 코드를 입력하세요
-SELECT outs.animal_id, outs.name FROM animal_outs as outs
-LEFT JOIN animal_INS as ins
-ON ins.animal_id = outs.animal_id
-WHERE ins.intake_condition is NULL
-ORDER BY animal_id;
+SELECT animal_id, name
+FROM animal_outs as outs
+WHERE animal_id not in (
+                    SELECT animal_id
+                    FROM animal_ins)
+order by animal_id
