c98d892dbd7670157f53ab3236ef378a231cab20
박주현 (Park Juhyun) <97617731+pjuju@users.noreply.github.com>
2023-10-10 14:28:31 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,46 +1,26 @@
-# 1. 각 왼쪽위 지점 4개가 붙어있고 색이 같을 때 합칠 수 있음
 import sys
-input = sys.stdin.readline
 
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-check = [[1] * N for _ in range(N)]
+N = int(sys.stdin.readline())
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
 
-blue = white = 0
-for r in range(N):
-    for c in range(N):
-        if arr[r][c] == 1:
-            blue += 1
-        else:
-            white += 1
+result = []
 
-x = 1
-l = 0
-while x != N:
-    x *= 2
-    l += 1
-
-for a in range(1, l+1):
-    for r in range(0, N, 2**a):
-        for c in range(0, N, 2**a):
-            for nr, nc in ((r+2**(a-1), c), (r,c+2**(a-1)), (r+2**(a-1), c+2**(a-1))):
-                if not check[nr][nc] or arr[r][c] != arr[nr][nc]:
-                    check[r][c] = 0
-                    break
-            if check[r][c]:
-                if arr[r][c]:
-                    blue -= 3
-                else:
-                    white -= 3
-                    
-print(white)
-print(blue)
+def solution(x, y, N) :
+  color = paper[x][y]
+  for i in range(x, x+N) :
+    for j in range(y, y+N) :
+      if color != paper[i][j] :
+        solution(x, y, N//2)
+        solution(x, y+N//2, N//2)
+        solution(x+N//2, y, N//2)
+        solution(x+N//2, y+N//2, N//2)
+        return
+  if color == 0 :
+    result.append(0)
+  else :
+    result.append(1)
 
 
-            
-
-
-
-
-
-
+solution(0,0,N)
+print(result.count(0))
+print(result.count(1))
